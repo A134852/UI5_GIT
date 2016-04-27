350757fc5ea45ad0d3f6d57535d1c46552d88be2
A134852 <fwu@agl.com.au>
2016-04-27 13:25:53 +1000

    Initial Checkin...

---
+++ ZDSTTEST_UI5_GIT/WebContent/zdsttest_ui5_git/git.view.js
@@ -0,0 +1,24 @@
+sap.ui.jsview("zdsttest_ui5_git.git", {
+
+	/** Specifies the Controller belonging to this View. 
+	* In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
+	* @memberOf zdsttest_ui5_git.git
+	*/ 
+	getControllerName : function() {
+		return "zdsttest_ui5_git.git";
+	},
+
+	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
+	* Since the Controller is given to this method, its event handlers can be attached right away. 
+	* @memberOf zdsttest_ui5_git.git
+	*/ 
+	createContent : function(oController) {
+ 		return new sap.m.Page({
+			title: "Title",
+			content: [
+			
+			]
+		});
+	}
+
+});
